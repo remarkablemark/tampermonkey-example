4898bd3c16f08962249203393a76548827d40eae
Mark <remarkablemark@users.noreply.github.com>
2022-11-16 12:42:24 -0500

    feat: add example.js

---
+++ example.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name        Tampermonkey Example
+// @namespace   https://www.tampermonkey.net/
+// @version     1.0.0
+// @description Tampermonkey example.
+// @match       https://example.com/
+// ==/UserScript==
+
+alert('Hello, world!');
